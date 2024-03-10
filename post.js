d2efbe959cd111f8c44bebdfb821893910ca6235
Matt <160410314+maturuuu@users.noreply.github.com>
2024-03-10 17:18:23 +0800

    Initial files after MCO1

---
+++ post.js
@@ -0,0 +1,21 @@
+//Post options menu
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleMenuButton = document.getElementById("post-options-button");
+    const optionsMenu = document.getElementById("post-options-menu");
+
+    toggleMenuButton.addEventListener("click", function() {
+        optionsMenu.classList.toggle("show"); // Toggle the 'show' class on the menu
+    });
+});
+
+//comment options menu
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleMenuButtons = document.querySelectorAll(".comment-options-button");
+    const optionsMenus = document.querySelectorAll(".comment-options-menu");
+
+    toggleMenuButtons.forEach(function(toggleMenuButton, index) {
+        toggleMenuButton.addEventListener("click", function() {
+            optionsMenus[index].classList.toggle("show"); // Toggle the 'show' class on the menu
+        });
+    });
+});
