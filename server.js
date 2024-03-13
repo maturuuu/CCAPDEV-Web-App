bbd0c35b1d92d16905552bc567fa2e666348fe36
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2024-03-13 14:36:06 +0800

    Update server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express()
+
+
+//default route to homepage
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '\\' + 'nonRegMainView.html')
+});
+
+//add routes here
+
+
+app.use(express.static(__dirname)); //allows use of static files like css
+
+var server = app.listen(5000, function(){
+    console.log("Web App running at port 5000")
+});
+
