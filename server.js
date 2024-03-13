eea75cfe43ecfa31b9302a4d5cf9fec4b7c81549
roaaaron <162535294+roaaaron@users.noreply.github.com>
2024-03-13 22:38:10 +0800

    with login  and register

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/rmv', function(req, res){
     res.sendFile(__dirname + '//' + 'RegMainView.html')
 });
 
+app.get('/register', function(req, res){
+    res.sendFile(__dirname + '//' + 'RegisterView.html')
+});
+
+app.get('/login', function(req, res){
+    res.sendFile(__dirname + '//' + 'LoginView.html')
+});
+
 app.use(express.static(__dirname)); //allows use of static files like css
 
 var server = app.listen(3000, function(){
