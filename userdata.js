5fa78c99246c9ddf337656e01c4eb994378557d8
roaaaron <162535294+roaaaron@users.noreply.github.com>
2024-03-15 00:26:27 +0800

    converted to js file and variables for export

---
+++ userdata.js
@@ -0,0 +1,36 @@
+let userlist = [
+    {
+        username: "",
+        profilepic: "jasmine.jpg",
+        firstname: "",
+        lastname: "",
+        posts: [],
+        totallikes: 0
+    },
+    {
+        username: "@matthew",
+        profilepic: "matthew.jpg",
+        firstname: "matthew",
+        lastname: "ong",
+        posts: [],
+        totallikes: 47
+    },
+    {
+        username: "@mjasmine",
+        profilepic: "jasmine.jpg",
+        firstname: "jasmine",
+        lastname: "gayamo",
+        posts: [],
+        totallikes: 57
+    },
+    {
+        username: "@jannah",
+        profilepic: "jannah.jpg",
+        firstname: "jannah",
+        lastname: "facultad",
+        posts: [],
+        totallikes: 27
+    }
+];
+
+module.exports = userlist;
