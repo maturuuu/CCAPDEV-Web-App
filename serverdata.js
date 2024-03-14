862346037f407904b73d453419e825329eeff251
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2024-03-14 17:17:02 +0800

    Update serverdata.js

---
+++ serverdata.js
@@ -0,0 +1,27 @@
+let posts = [
+    {
+        id: 1,
+        authorid: 1,
+        title: "This is my user-generated post",
+        content: "User generated content! Hooray!",
+        timestamp: "Placeholder",
+        isedited: "edited",
+        comments: [
+            {
+                id: 11,
+                authorid: 2,
+                content: "This is my user-generated reply to the post :))",
+                isedited: "edited",
+                isreply: false
+            },
+            {
+                id: 12,
+                authorid: 1,
+                content: "this is a reply to THAT reply MUWAHAHAHA",
+                isedited: "",
+                isreply: true
+            },
+        ]
+    }
+];
+//add likes later, make it so if current user id matches author id, you can edit.
