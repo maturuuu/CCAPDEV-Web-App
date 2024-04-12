81c6f7177de6357c83dbada73890302b56d5afd8
roaaaron <162535294+roaaaron@users.noreply.github.com>
2024-04-12 21:29:13 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     authorname: String,
     authorusername: String,
+    authoremail: String,
+    authorpassword: String,
     authorimg: String,
     authorbio: String,
 })
